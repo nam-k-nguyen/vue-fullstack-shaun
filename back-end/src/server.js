ee12b264aca5fe59ac8b97117102dcb15f5862d0
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-19 01:30:50 -0400

    Update dist

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -69,7 +69,7 @@ async function start() {
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../dist/index.html'));
-    })
+    });
 
     const port = process.env.PORT || 8000;
 
